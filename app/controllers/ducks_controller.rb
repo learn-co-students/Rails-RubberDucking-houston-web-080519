0d942789739cb925268376ac4e5109148e1718e6
BarretteBanner <barrettebanner@gmail.com>
2019-09-04 15:41:14 -0500

    done

--- app/controllers/ducks_controller.rb
+++ app/controllers/ducks_controller.rb
@@ -4,4 +4,40 @@ class DucksController < ApplicationController
     @ducks = Duck.all
   end
 
+  def show 
+    @students = Student.all
+    @duck = Duck.find(params[:id])
+  end
+
+  def new
+    @students = Student.all
+    student = @students.sample
+  end
+
+  def create
+    @students = Student.all
+    student = @students.sample
+    Duck.create({
+      name: params[:name],
+      description: params[:description],
+      student_id: student.id
+    })
+    redirect_to "/ducks/index"
+  end
+
+  def edit
+    @duck = Duck.find(params[:id])
+  end
+
+  def update
+    @duck = Duck.find(params[:id])
+    @duck.update({
+      name: params[:name],
+      description: params[:description],
+    })
+    redirect_to '/ducks/'
+  end
+
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
   resources :ducks, only: [:index]
   resources :students, only: [:index]
+  get "students/index", to: "students#index" 
+  get "students/new", to: "students#new"
+  post "students/new", to: "students#create"
+  get "students/show/:id", to: "students#show"
+  get "students/edit/:id", to: "students#edit"
+  patch "students/edit/:id", to: "students#update"
+  get "ducks/index", to: "ducks#index"
+  get "ducks/new", to: "ducks#new"
+  post "ducks/new", to: "ducks#create"
+  get "ducks/show/:id", to: "ducks#show"
+  get "ducks/edit/:id", to: "ducks#edit"
+  patch "ducks/edit/:id", to: "ducks#update"
+  
 end
